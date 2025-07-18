0d2786b5334e1b78b14ad31d31c0338e5eec8b0a
olietchecopar <oliviaetchecopar@hotmail.com>
2025-07-18 16:29:11 -0300

    commit

--- dist/tipo-animal.js
+++
@@ -1,8 +0,0 @@
-import crypto from 'node:crypto';
-export class TipoAnimal {
-    constructor(nombre, codigoTipo = crypto.randomUUID()) {
-        this.nombre = nombre;
-        this.codigoTipo = codigoTipo;
-    }
-}
-//# sourceMappingURL=tipo-animal.js.map
